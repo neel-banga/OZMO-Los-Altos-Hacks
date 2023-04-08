018d1f583ee82b8a33a79d4b0358686c77541e2f
Neel <84484122+neel-banga@users.noreply.github.com>
2023-04-08 12:22:05 -0700

    Create text.py

---
+++ text.py
@@ -0,0 +1,20 @@
+import os
+from twilio.rest import Client
+from audio import listen
+
+# Set up the Twilio client
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+
+text = listen()
+
+message = client.messages.create(
+    to = '+19255776636',
+    from_ = '+18339952474', 
+    body = text 
+)
+
+# Print the message SID
+print(message.sid)
